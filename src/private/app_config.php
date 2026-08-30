293d285a31a157be54ce17f5464f5da53982e5d0
tooyamanaohide <naohide.tooyama@system-kobo.com>
2026-08-30 09:54:37 +0900

    管理者アカウントを直書きに変更

--- src/private/app_config.php
+++ src/private/app_config.php
@@ -1,25 +1,7 @@
 <?php
 
-$local = [];
-$localPath = __DIR__ . '/app_config.local.php';
-if (is_file($localPath)) {
-	$loaded = require $localPath;
-	if (is_array($loaded)) {
-		$local = $loaded;
-	}
-}
-
-$envOrLocal = static function (string $envKey, string $localKey, array $local): string {
-	$envValue = $_ENV[$envKey] ?? getenv($envKey);
-	if ($envValue !== false && $envValue !== null && $envValue !== '') {
-		return (string)$envValue;
-	}
-
-	return (string)($local[$localKey] ?? '');
-};
-
 return [
-	'google_api_key' => $envOrLocal('GOOGLE_API_KEY', 'google_api_key', $local),
-	'admin_user' => $envOrLocal('ADMIN_USER', 'admin_user', $local),
-	'admin_password_hash' => $envOrLocal('ADMIN_PASSWORD_HASH', 'admin_password_hash', $local),
+	'google_api_key' => 'AIzaSyD0DLkQ3hG9i8oecOapgk0dIRjUUIJJ4gY',
+	'admin_user' => 'adminpp',
+	'admin_password_hash' => '$2y$12$3aJgXWe8CjqNwsgKPHdMJO2ncXgMr.BiVXs8vSNIF9D5OoLPsca/K',
 ];
